5ce9f71de189944865a8d2565e249d3a5b58f86c
mildminihi <mildsupanat@hotmail.com>
2017-11-13 21:47:38 +0700

    test case email is empty

---
+++ app/src/test/java/cc/somkiat/basicunittesting/EmailValidationFailTest.java
@@ -0,0 +1,19 @@
+package cc.somkiat.basicunittesting;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+
+/**
+ * Created by mild supanat on 13/11/2560.
+ */
+
+public class EmailValidationFailTest {
+
+    @Test
+    public void emailIsEmpty(){
+        EmailValidation emailValidation = new EmailValidation();
+        boolean result = emailValidation.isEmpty("");
+        assertFalse("ต้องไม่ผ่านเพราะEMAILเป็นค่าว่าง", result);
+    }
+}
