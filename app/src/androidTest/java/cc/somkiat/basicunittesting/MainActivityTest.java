5a71cf3ae79c3fb6923d403dc3ed0bcf85690475
mildminihi <mildsupanat@hotmail.com>
2017-11-14 02:47:53 +0700

    UI save with empty Email test

--- app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
@@ -1,15 +1,10 @@
 package cc.somkiat.basicunittesting;
 
 
-import android.os.IBinder;
-import android.support.test.espresso.Root;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.WindowManager;
 
-import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,34 +43,21 @@ public class MainActivityTest {
 
     @Test
     public void saveWithEmptyName(){
-        
+
         onView(withId(R.id.emailInput)).perform(replaceText("Mildsupanat@example.com"), closeSoftKeyboard());
         onView(allOf(withId(R.id.saveButton), withText("Save"))).perform(click());
 //        onView(withText("Validation Result: Name Validation Success And Email Validation Success")).check(matches(isDisplayed()));
     }
 
 
+    @Test
+    public void saveWithEmptyEmail(){
+        onView(withId(R.id.userNameInput)).perform(replaceText("Mild Supanat"), closeSoftKeyboard());
+        onView(allOf(withId(R.id.saveButton), withText("Save"))).perform(click());
+//        onView(withText("Validation Result: Name Validation Success And Email Validation Success")).check(matches(isDisplayed()));
+    }
+
 
 
 }
 
-class ToastMatcher extends TypeSafeMatcher<Root> {
-
-    @Override
-    public void describeTo(Description description) {
-        description.appendText("is toast");
-    }
-
-    @Override
-    public boolean matchesSafely(Root root) {
-        int type = root.getWindowLayoutParams().get().type;
-        if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
-            IBinder windowToken = root.getDecorView().getWindowToken();
-            IBinder appToken = root.getDecorView().getApplicationWindowToken();
-            if (windowToken == appToken) {
-                //means this window isn't contained by any other windows.
-            }
-        }
-        return false;
-    }
-}
